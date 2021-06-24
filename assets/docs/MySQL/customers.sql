f1b759670c65a616ec90d352f378824333450403
刘振 <547858171@qq.com>
2021-06-24 15:04:31 +0800

    MySQL学习

---
+++ assets/docs/MySQL/customers.sql
@@ -0,0 +1,16 @@
+########################
+# Create customers table
+########################
+CREATE TABLE IF NOT EXISTS customers
+(
+  cust_id      int       NOT NULL AUTO_INCREMENT,
+  cust_name    char(50)  NOT NULL ,
+  cust_address char(50)  NULL ,
+  cust_city    char(50)  NULL ,
+  cust_state   char(5)   NULL ,
+  cust_zip     char(10)  NULL ,
+  cust_country char(50)  NULL ,
+  cust_contact char(50)  NULL ,
+  cust_email   char(255) NULL ,
+  PRIMARY KEY (cust_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ assets/docs/MySQL/orders.sql
@@ -0,0 +1,10 @@
+#####################
+# Create orders table
+#####################
+CREATE TABLE orders
+(
+  order_num  int      NOT NULL AUTO_INCREMENT,
+  order_date datetime NOT NULL ,
+  cust_id    int      NOT NULL ,
+  PRIMARY KEY (order_num)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
